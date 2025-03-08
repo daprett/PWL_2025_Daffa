ae5a31eae8da765a37d2f852410b75819605f25a
daprett <athallahdaffa936@gmail.com>
2025-03-08 11:52:16 +0700

    Praktikum 2.5:Attribute Changes

--- Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
@@ -12,20 +12,21 @@ class UserController extends Controller
     
     public function index(){
       
-        // $user = UserModel::firstOrCreate([// otomatis menambah data yang tidak ada 
-        //     'username' => 'manager22',
-        //     'nama' => 'Manager Dua Dua',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => '2'
-        // ],);
-
-        $user = UserModel::firstOrNew([//harus melakukan save untuk menambah data yang tidak ada 
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => '2'
-        ],);
+        ]);
+
+        $user->username = 'manager12';
         $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged('username','level_id');
+        $user->wasChanged('nama');
+        dd($user->wasChanged('nama','username'));
 
         return view('user', ['data' => $user]); // Kirim ke tampilan
      
@@ -49,6 +50,46 @@ class UserController extends Controller
          // $user = UserModel::where('username','manager9')->firstOrFail();
 
         //  $user = UserModel::where('level_id', 2)->count(); Hitung jumlah user
+
+                // $user = UserModel::firstOrCreate([// otomatis menambah data yang tidak ada 
+        //     'username' => 'manager22',
+        //     'nama' => 'Manager Dua Dua',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => '2'
+        // ],);
+
+        // $user = UserModel::firstOrNew([//harus melakukan save untuk menambah data yang tidak ada 
+        //     'username' => 'manager33',
+        //     'nama' => 'Manager Tiga Tiga',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => '2'
+        // ],);
+        // $user->save();
+
+                // $user = UserModel::firstOrCreate([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => '2'
+        // ],);
+
+        // $user->username = 'manager56';
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama','username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama','username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+
+        // dd($user->isDirty());
 
         // $user = UserModel::all();
         // return view('user', ['data'=>$user]);
