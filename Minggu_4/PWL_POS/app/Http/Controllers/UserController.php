aa4fd9046af1d9ec05ba2483c2daac062f2fe753
daprett <athallahdaffa936@gmail.com>
2025-03-08 09:52:29 +0700

    Praktikum 2.1:Retrieving Single Models

--- Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
@@ -11,18 +11,24 @@ class UserController extends Controller
 {
     
     public function index(){
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+            abort(404);
+        });
+       
+        return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => '2',
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $user = UserModel::firstwhere('level_id',1);
+                // $data = [
+        //     'level_id' => '2',
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
 
-        UserModel::create($data);
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data'=>$user]);
+        // $user = UserModel::all();
+        // return view('user', ['data'=>$user]);
     }
 }
