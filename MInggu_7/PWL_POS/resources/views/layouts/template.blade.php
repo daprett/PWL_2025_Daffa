a2f9e1decd62c69daf2384de59f018635b393d9a
daprett <athallahdaffa936@gmail.com>
2025-04-04 17:10:21 +0700

    rapikan

--- MInggu_7/PWL_POS/resources/views/layouts/template.blade.php
+++ MInggu_7/PWL_POS/resources/views/layouts/template.blade.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{config('app.name','PWL Laravel Satrter Code') }}</title>
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
  <meta name="csrf-token" content="{{csrf_token()}}"> {{-- untuk mengirimkan token laravel setiap csrf setiap req ajax --}}
 
