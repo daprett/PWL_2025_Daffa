ab523a3195d5886f76fc9abab74c058bb25dd6e8
daprett <athallahdaffa936@gmail.com>
2025-04-21 16:20:16 +0700

    JS 10

---
+++ Minggu_10/PWL_POS/app/Models/UserModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class UserModel extends Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id'; // mendefinisi primary key dari tabel
+    
+    protected $fillable=['level_id','username','nama','password', 'created_at', 'updated_at', 'profile_picture'];
+
+    protected $hidden = ['password']; //jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; //casting password agar otomatis di hash
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function getRoleName(): string{
+        return $this->level->level_nama;
+    }
+
+    public function hasRole($role): bool{
+        return $this->level->level_kode == $role;
+    }
+
+    public function getRole() {
+        return $this->level->level_kode;
+    }
+}
